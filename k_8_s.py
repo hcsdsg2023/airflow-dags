ef799749b7b6255a000bf045c803d8c93f5ba7eb
hcsdsg2023 <122088814+hcsdsg2023@users.noreply.github.com>
2023-01-08 17:09:40 +0530

    Create k_8_s.py

---
+++ k_8_s.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.kubernetes_pod_operator import KubernetesPodOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'me',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10)
+)
+
+task = KubernetesPodOperator(
+    namespace='test',
+    image="bash:4.4.23",
+    cmds=["bash", "-cx"],
+    arguments=["echo", "10"],
+    labels={"foo": "bar"},
+    name="task-one",
+    task_id="task-one",
+    get_logs=True,
+    dag=dag,
+    in_cluster=True
+)
