5166e3a01be8c0d1ba10505a84acde98b31c6754
hcsdsg2023 <122088814+hcsdsg2023@users.noreply.github.com>
2023-01-08 17:36:00 +0530

    Update k_8_s.py

--- k_8_s.py
+++ k_8_s.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from datetime import datetime, timedelta
 
 default_args = {
